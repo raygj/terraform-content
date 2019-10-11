91e5750497b05844213f570c8009f3c498fde77e
Jim Ray <ray.jim@gmail.com>
2019-10-11 16:46:16 -0400

    updated esxi main.tf

---
+++ esxi/main.tf
@@ -0,0 +1,40 @@
+provider "esxi" {
+  esxi_hostname = "192.168.1.228"
+  esxi_hostport = "22"
+  esxi_username = "root"
+  esxi_password = "<password>" //look into pulling this from a var
+}
+
+resource "esxi_guest" "homelab" {
+  guest_name = "${var.guest_name}"
+  disk_store = "${var.disk_store}"
+  memsize    = "${var.memsize}"
+  numvcpus   = "4"
+  power      = "on"
+  notes      = "hashistack node ${var.guest_number}"
+
+  #
+  #  Specify an existing guest to clone, an ovf source, or neither to build a bare-metal guest vm.
+  #
+  #ovf_source        = "/local_path/${var.os}.vmx"
+
+  clone_from_vm = "${var.os}-template"
+  network_interfaces = [
+    {
+      virtual_network = "VM Network"
+      nic_type    = "vmxnet3"
+    },
+  ]
+
+provisioner "file" {
+    source      = "/templates/hashistack_setup.sh"
+    destination = "/tmp/hashistack_setup.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/hashistack_setup.sh",
+      "./tmp/hashistack_setup.sh",
+    ]
+  }
+}
---
+++ esxi/variables.tf
@@ -0,0 +1,21 @@
+variable "guest_name" {
+  description = "full name of VM"
+}
+
+variable "disk_store" {
+  description = "SSD or SATA"
+  default = "SSD"
+}
+
+variable "memsize" {
+  description = "amount of RAM in GB"
+}
+
+variable "guest_number" {
+  description = "used in notes field, should match guest name"
+}
+
+variable "os" {
+  description = "centos or ubuntu, used to pick template"
+  default = "ubuntu"
+}
