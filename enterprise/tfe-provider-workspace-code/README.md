8d012c4acb9d6221e1dffdb01caf3955c46743ff
Jim Ray <ray.jim@gmail.com>
2020-01-13 20:01:22 -0500

    added TF code for TFE workspace

---
+++ enterprise/tfe-provider-workspace-code/README.md
@@ -0,0 +1,4 @@
+use the Terraform Enterprise provider to configure your TFC or TFE workspaces using TF CLI (OSS)
+
+https://www.terraform.io/docs/providers/tfe/index.html
+
---
+++ enterprise/tfe-provider-workspace-code/workspace.tf
@@ -0,0 +1,37 @@
+provider "tfe" {
+  hostname = "${var.hostname}"
+  token    = "${var.token}"
+}
+
+# workspace creation
+
+resource "tfe_workspace" "tfe-deploy" {
+  name         = "{var.tfe-deploy-workspace-name}"
+  organization = "{var.org-name}"
+  terraform_version = "{var.terraform_ver}"
+  working_directory = "{var.working_dir}"
+  vcs_repo =
+    identifier = "{var.org-repo}" # must be in format org/repo
+    oauth_token_id = "{var.vcs-oauth}"
+}
+
+# notification configuration
+
+resource "tfe_organization" "tfe-deploy" {
+  organization = "{var.org-name}"
+  email = "{var.prefix@hashicorp.com}"
+}
+
+resource "tfe_workspace" "tfe-deploy" {
+  name         = "{var.tfe-deploy-workspace-name}"
+  organization = "{var.org-name}"
+}
+
+resource "tfe_notification_configuration" "tfe-deploy" {
+  name                      = "jray-tfe-slack"
+  enabled                   = true
+  destination_type          = "slack"
+  triggers                  = ["run:created", "run:planning", "run:needs_attention", "run:applying", "run:completed", "run:errored"]
+  url                       = "https://hooks.slack.com/services/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
+  workspace_external_id     = "${tfe_workspace.test.external_id}"
+}
